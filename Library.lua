f3c91a45ef1b1e1cd4684be6c0c32ee5b2d9f084
Radiatee <dziwanifilip74@gmail.com>
2026-06-22 04:12:56 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -219,9 +219,9 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
+        BackgroundColor = Color3.fromRGB(0, 0, 0),
         MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
+        AccentColor = Color3.fromRGB(255, 91, 0),
         OutlineColor = Color3.fromRGB(40, 40, 40),
         FontColor = Color3.new(1, 1, 1),
         Font = Font.fromEnum(Enum.Font.Code),
